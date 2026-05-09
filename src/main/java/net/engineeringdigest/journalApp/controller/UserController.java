56d25fb425402c8a2e1486ae16077b672ec45e72
Ishmam07 <ishmamtahmid8@gmail.com>
2026-05-10 03:16:41 +0600

    Added WeatherStack API integration and WeatherResponse

--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -1,9 +1,13 @@
 package net.engineeringdigest.journalApp.controller;
 
+import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.service.UserService;
+import net.engineeringdigest.journalApp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private WeatherService weatherService;
 
     @PutMapping
     public void updateUser(@RequestBody User user) {
@@ -32,6 +39,17 @@ public class UserController {
     public void deleteUserById() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         userRepository.deleteByUsername(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<?> greetings() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        WeatherResponse weatherResponse = weatherService.getWeather("Dhaka");
+        String greeting = "";
+        if(weatherResponse != null) {
+            greeting = ", Weather feeld like " + weatherResponse.getCurrent().getFeelslike() + ", Actual Temperature " + weatherResponse.getCurrent().getTemperature();
+        }
+        return new ResponseEntity<>("HI " + authentication.getName() + greeting, HttpStatus.OK);
     }
 
 }
