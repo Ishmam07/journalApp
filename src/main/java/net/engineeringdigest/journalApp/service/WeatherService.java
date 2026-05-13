e5860162e0a9b1381b10fce3ec0d2caaa1e6060d
Ishmam07 <ishmamtahmid8@gmail.com>
2026-05-13 16:54:59 +0600

    Added App Cache

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -1,7 +1,9 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import net.engineeringdigest.journalApp.cache.AppCache;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -9,14 +11,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class WeatherService {
-    private static final String apiKey = "c662efba7d1c09b3ebec381e3139207d";
-    private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+
+    @Value("${weather.api.key}")
+    private String apiKey;
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private AppCache appCache;
+
     public WeatherResponse getWeather(String city) {
-        String finalAPI = API.replace("CITY", city).replace("API_KEY", apiKey);
+        String finalAPI = appCache.APP_CACHE.API.replace("CITY", city).replace("API_KEY", apiKey);
         ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
         WeatherResponse body = response.getBody();
         return body;
